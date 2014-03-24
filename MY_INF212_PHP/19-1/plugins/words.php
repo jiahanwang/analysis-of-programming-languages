a0330e79cea8637d3484b781c1eac8f655ac6dd0
jiahanwang <jiahanwng1990@gmail.com>
2014-03-23 20:45:32 -0700

    update

--- MY_INF212_PHP/19-1/plugins/words.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function extract_words($path, $stop_path){
-		$words = preg_split("/\s+/", trim(strtolower(preg_replace('/[\W]+/', ' ', file_get_contents($path)))));
-		$stop_words = preg_split('/,/', file_get_contents($stop_path));
-		return array_diff($words, array_merge($stop_words, range('a', 'z')));
-}
